f0196f39dfd25b80743113834aaf5dd4bd16db6d
depsep <marondon@studentmail.conroeisd.net>
2025-05-20 13:53:20 +0000

    Pending changes exported from your codespace

--- test stuff/circlearea/main.py
+++ test stuff/circlearea/main.py
@@ -1,14 +1,15 @@
 import math
 
 size = float
-#should be 25 sq meters.
-#31 is close enough, finished!
+#should be 25 squared units for 5 width.
+#31 is close enough, epic!
 
 def main():
     print("Hello world")
     size = float(input("Choose the size of the circle in units. (FROM CENTER) = "))
     print(f" Area of a {size} unit wide circle:")
-    print(f"{round(math.pi * size**2)} square units")
+    print(f"{round(math.pi * (size*size))} square units")
+    print(f"{math.pi * size**2} sq. units UNROUNDED")
     print(f"pi = {math.pi}")
 
 main()
